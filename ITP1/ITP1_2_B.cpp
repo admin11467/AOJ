2a3947c77110b940587ece0510f379687eb5779f
Admin11467 <adm11467.dev@gmail.com>
2026-04-03 23:07:12 +0900

    Day5

---
+++ ITP1/ITP1_2_B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int a,b,c;
+    std::cin >> a >> b >> c;
+
+    if (a < b) {
+        if (b < c) {
+            std::cout << "Yes" << std::endl;
+        } else {
+            std::cout << "No" << std::endl;
+        }
+    } else {
+        std::cout << "No" << std::endl;
+    }
+}
