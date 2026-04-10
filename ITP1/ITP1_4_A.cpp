f2fc3b5d8df8446e856fb0af01aea62d18080213
Admin11467 <adm11467.dev@gmail.com>
2026-04-10 23:12:33 +0900

    Add .gitignore, remove CMakeLists.txt, and set up VSCode configuration

---
+++ ITP1/ITP1_4_A.cpp
@@ -0,0 +1,15 @@
+#include <ios>
+#include <iostream>
+#include <iomanip>
+
+int main(){
+    int a,b,d,r;
+    double f;
+    std::cin >> a >> b;
+
+    d = a/b;
+    r = a%b;
+    f = 1.0 * a/b;
+
+    std::cout << d << " " << r << " " << std::fixed << std::setprecision(5) << f << std::endl;
+}
