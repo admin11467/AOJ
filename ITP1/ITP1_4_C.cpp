96e5d280db8b1a83a074e16602830bd80f9db81e
Admin11467 <adm11467.dev@gmail.com>
2026-04-12 20:55:04 +0900

    ITP1_4_C

---
+++ ITP1/ITP1_4_C.cpp
@@ -0,0 +1,41 @@
+#include <cstddef>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(){
+    int a,b;
+    std::vector<int> result;
+    std::string op;
+
+    while (true) {
+
+        std::cin >> a >> op >> b;
+
+        if(op == "+"){
+            result.push_back(a + b);
+        }
+
+        if(op == "-"){
+            result.push_back(a - b);
+        }
+
+        if(op == "*"){
+            result.push_back(a * b);
+        }
+
+        if(op == "/"){
+            result.push_back(a / b);
+        }
+
+        if(op == "?"){
+            break;
+        }
+    
+    }
+
+    for(std::size_t i = 0;i < result.size(); i++){
+        std::cout << result[i] << std::endl;
+    }
+    
+}
