9f22ea40661eabbad362b9706e9a2f154b3170e8
Admin11467 <adm11467.dev@gmail.com>
2026-04-14 20:54:47 +0900

    ITP1_5_A

---
+++ ITP1/ITP1_5_A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<int> h,w;
+
+    while (true) {
+        int hcin,wcin;
+        std::cin >> hcin >> wcin;
+        
+        if (hcin == 0 && wcin == 0){
+            break;
+        } else {
+            h.push_back(hcin);
+            w.push_back(wcin);
+        }
+
+    }
+
+    for (size_t x =0; x < h.size(); x++){
+
+        for(int hi = 0; hi < h[x]; hi++){
+
+            for(int wi =0; wi < w[x]; wi++){
+                std::cout << "#"; 
+            }
+            std::cout << std::endl;
+
+        }
+
+        std::cout << std::endl;
+
+    }
+
+}
