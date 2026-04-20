0558de839ccd6c66333319c3f26d75d0dc9e44d6
Hikaru <hikaru@natuke.net>
2026-04-20 23:10:50 +0900

    ITP1_6_A

---
+++ ITP1/ITP1_6_A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    int n;
+    std::cin >> n;
+    std::vector<int> a;
+
+    for(int i = 0;i < n;i++){    
+
+    }
+
+    for(size_t i = a.size();i > 0;i = i - 1){
+        std::cout << a[i-1];
+        if (i != 1){std::cout << " ";}
+    }
+    std::cout << std::endl;
+
+}        int temp;
+        std::cin >> temp;
+        a.push_back(temp);
